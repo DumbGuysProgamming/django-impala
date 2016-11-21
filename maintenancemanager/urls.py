b08388adef59eb15f0bf5022cb6cdd00b876dbf6
slobbishbody <kyle.pitzen@gmail.com>
2016-11-21 15:08:57 -0600

    Added some new views

--- maintenancemanager/urls.py
+++ maintenancemanager/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     #index
     url(r'^$', views.index, name='index'),
     #vehicle
-    url(r'^(?P<vehicle_id>[0-9]+)/vehicle/$', views.vehicle_information, name='vehicle'),
+    # url(r'^(?P<vehicle_id>[0-9]+)/vehicle/$', views.vehicle_information, name='vehicle'),
     #client
     url(r'^(?P<client_id>[0-9]+)/client/$', views.client_information, name='client'),
     #service
-    url(r'^(?P<service_id>[0-9]+)/service/$', views.service_information, name='service'),
+    # url(r'^(?P<service_id>[0-9]+)/service/$', views.service_information, name='service'),
 ]
--- maintenancemanager/views.py
+++ maintenancemanager/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
 
 from .models import *
 
 def index(request):
+    '''Displays a bulleted list of all clients'''
     client_data_list = Client.objects.order_by('-last_name')
-    template = loader.get_template('maintenancemanager/index.html')
     context = {
         'client_data_list': client_data_list
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'maintenancemanager/index.html', context)
 
 def client_information(request, client_id):
-    client_data = Client.objects.get(pk=client_id)
-    client_name = '{}, {}'.format(client_data.last_name, client_data.first_name)
-    return HttpResponse("You're looking at information for client: %s." % client_name)
+    '''this method tries to get client data by id.  If none is found, raises an error'''
+    try:
+        client_data = Client.objects.get(pk=client_id)
+    except Client.DoesNotExist:
+        raise Http404("Client does not exist")
+    last_name = client_data.last_name
+    first_name = client_data.first_name
+    return render(request, 'maintenancemanager/client_information.html', {'last_name': last_name, 'first_name': first_name})
 
-def service_information(request, service_id):
-    return HttpResponse("You're looking at information for service: %s." % service_id)
-
-def vehicle_information(request, vehicle_id):
-    return HttpResponse("You're looking at information for vehicle: %s." % vehicle_id)
